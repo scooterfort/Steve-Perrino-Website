c65edc1850b58d9da14d0072114a34d2ff797cbb
scooterfort <scooterfort@gmail.com>
2022-06-27 10:40:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+//PARALLAX
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.6 + "px";
+})
+//PARALLAX END
+
+// NAV BAR
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        // toggle nav
+        nav.classList.toggle('nav-active');
+
+        // animate links 
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
+        // burger animation
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+
+
+navSlide();
+
+//NAV BAR END
