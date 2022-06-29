b84fbacbe1ecbc6792af6013620ec3705e93c464
scooterfort <scooterfort@gmail.com>
2022-06-29 11:33:54 -0400

    p font color change

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ window.addEventListener("scroll", function () {
 //PARALLAX END
 
 //NAV BAR
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
 
 
-    burger.addEventListener('click', () => {
-        // toggle nav
-        nav.classList.toggle('nav-active');
+//     burger.addEventListener('click', () => {
+//         // toggle nav
+//         nav.classList.toggle('nav-active');
 
-        // animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
+//         // animate links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             }
+//             else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+//             }
+//         });
 
-        // burger animation
-        burger.classList.toggle('toggle');
+//         // burger animation
+//         burger.classList.toggle('toggle');
 
-    });
-}
+//     });
+// }
 
 
 
